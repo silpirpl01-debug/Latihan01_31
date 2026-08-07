837639f0f5d80a2be6bd9269ca632fa6f0d07986
Silvi Oktavia <silpi.rpl01@gmail.com>
2026-08-07 08:39:12 +0700

    menambahkan fungsi hitung total dan hitungAkhir

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,46 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+
+//FUNGSI UNTUK MENGHITUNG TOTAL BELANJA
+double hitungTotal(int jumlah, double harga) {
+  return jumlah * harga;
+}
+
+// FUNGSI MENGHTUNG TOTAL AKHIR SETELAH POTONGAN
+double hitungHargaAkhir(double total, double persenPotongan) {
+  return total - (total * persenPotongan / 100);
+}
 
 void main() {
-  int stok = 3;
+  //Data transaksi 
+  int jumlah = 5;
+  double harga = 3000;
 
-  print("--- Penjualan Buku Tulis ---");
+  //memanggil fungsi 
+  double total = hitungTotal(jumlah, harga);
 
-  while (stok > 0) {
-    stok--;
-    print("Terjual 1, sisa stok: $stok");
+  //menentukan potongan sesuai aturan koperasi
+  double potongan;
+
+  if  (total > 200000) {
+    potongan = 10;
+  } else if (total > 100000) {
+    potongan = 5;
+  } else {
+    potongan = 0;
   }
 
-  // Bahaya jika kondisi berhenti pada while keliru:
-  // Program dapat terus berjalan tanpa henti (infinite loop)
-  // atau menjual barang melebihi stok yang tersedia.
-  //
-  // Cara memastikan koperasi tidak menjual melebihi stok:
-  // Gunakan kondisi while (stok > 0) dan kurangi nilai stok
-  // setiap kali terjadi penjualan (stok--), sehingga penjualan
-  // akan berhenti saat stok mencapai 0.
+  // Menghitung harga akhir setelah potongan
+  double hargaAkhir = hitungHargaAkhir(total, potongan);
+
+  //Menampilkan hasil total belanja
+  print("=== TRANSAKSI KOPERASI ===");
+  print("Jumlah Barang : $jumlah");
+  print("Harga Satuan : Rp${harga.toStringAsFixed(0)}");
+  print("Total Belanja : Rp${total.toStringAsFixed(0)}");
+  print("Potongan : $potongan%");
+  print("Total Bayar : Rp${hargaAkhir.toStringAsFixed(0)}");
 }
+
+/*
+Program dibagi menjadi fungsi agar lebih mudah dipelihara. Jika aturan potongan berubah, cukup mengubah fungsi hitungHargaAkhir() atau logika penentuan potongan satu kali saja sehingga seluruh program langsung mengikuti aturan baru.
+*/
