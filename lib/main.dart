dc07f6adb4c5ab5dc461dc67be981982cbe71bc6
Silvi Oktavia <silpi.rpl01@gmail.com>
2026-08-10 09:33:21 +0700

    list barang

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,44 @@
 import "package:flutter/material.dart";
 
-class barang {
+class Barang {
   String nama;
   double harga;
   int stok;
 
-  barang(this.nama, this.harga, this.stok);
+  Barang(this.nama, this.harga, this.stok);
 
-  void tampilkaninfo(){
+  void tampilkan() {
     print("====================");
     print("KARTU BARANG");
-    print("Nama : $nama");
-    print("Harga  : Rp$harga.toStringAsFixed(0}");
-    print("stok : $stok");
-     print("====================");
+    print("Nama  : $nama");
+    print("Harga : Rp${harga.toStringAsFixed(0)}");
+    print("Stok  : $stok");
+    print("====================");
   }
 }
 
 void main() {
-  barang barang1 = barang("Buku tulis", 3000, 20);
-  barang barang2 = barang("pulpen", 2500, 15);
-  barang barang3 = barang("roti", 5000, 10);
+  Barang barang1 = Barang("Buku tulis", 3000, 20);
+  Barang barang2 = Barang("Pulpen", 2500, 15);
+  Barang barang3 = Barang("Roti", 5000, 10);
+
   print("=== DATA BARANG KOPERASI ===");
-  barang1.tampilkaninfo();
-  barang2.tampilkaninfo();
-  barang3.tampilkaninfo();
+
+  List<Barang> daftarBarang = [
+    barang1,
+    barang2,
+    barang3,
+  ];
+
+  for (Barang barang in daftarBarang) {
+    barang.tampilkan();
+  }
 }
+/*
+  Perbedaan dari Sprint 3:
+  Sebelumnya, data nama dan harga barang dibuat dalam List yang berbeda,
+  sehingga setiap data masih terpisah. Pada Sprint ini, informasi barang
+  seperti nama, harga, dan stok disatukan ke dalam class Barang.
+  Setiap barang kemudian disimpan dalam satu List<Barang>, sehingga
+  struktur data menjadi lebih teratur dan hubungan antar data lebih jelas.
+*/
